bf4e650a06051a25f94fdae0db8820873009ba94
AaronGitsHub <aaronwood5@mac.com>
2019-05-01 13:15:30 -0400

    Change NoteCreate Model

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -16,5 +16,16 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+
     }
 }
